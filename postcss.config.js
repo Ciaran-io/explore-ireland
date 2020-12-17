1118fee7d0de6d0131f0d863d7712dbdf39fc7b7
Ciaran <ciaran_toner@icloud.com>
2020-12-17 00:10:31 +0000

    Commented out purgecss plugin. issues with removing css classes that are being used.

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,12 @@
-const autoprefixer = require('autoprefixer');
-
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    // require('@fullhuman/postcss-purgecss')({
+    //   content: [
+    //     './index.html',
+    //   ],
+    //   defaultExtractor: content => content.match(/[A-Za-z0-9_:/]+/g) || []
+    // })
   ]
 } 
