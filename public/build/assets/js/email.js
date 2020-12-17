1b337225d0395797562234643a4d2503814744f1
Ciaran <ciaran_toner@icloud.com>
2020-12-17 00:16:59 +0000

    This file is used to accepct emails & user input in email section of page.

---
+++ public/build/assets/js/email.js
@@ -0,0 +1,22 @@
+const btn = document.getElementById('button-email');
+
+emailjs.init('user_ZxaX1Ph4GVNusJIcXdifR');
+
+document.getElementById('form')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    btn.value = 'Your message is sent, have a great day!';
+
+    const serviceID = 'default_service';
+    const templateID = 'template_k3subcm';
+
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'Send Email';
+        alert('Sent!');
+      }, (err) => {
+        btn.value = 'Send Email';
+        alert(JSON.stringify(err));
+      });
+  });
